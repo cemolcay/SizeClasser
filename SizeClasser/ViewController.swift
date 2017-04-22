04d5b53d1f2406c7a7d8c2cd1799692e511f4e16
cemolcay <ccemolcay@gmail.com>
2017-04-22 07:17:59 +0300

    Initial Commit

---
+++ SizeClasser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClasser
+//
+//  Created by Cem Olcay on 22/04/2017.
+//  Copyright © 2017 cemolcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
